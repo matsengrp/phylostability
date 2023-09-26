d3cc905047b7eff80f15ea034b3d7e428ee88be6
Lena Collienne <lena@lenacoll.de>
2023-09-26 16:27:07 -0700

    WIP: compute RF distances between reattached and full tree

--- scripts/extract_data_from_iqtree_runs.py
+++ scripts/extract_data_from_iqtree_runs.py
@@ -1,15 +1,17 @@
 from ete3 import Tree
 
 tree_file = snakemake.input[1]
-ml_file = snakemake.input[2]
+full_tree_file = snakemake.input[2]
+ml_file = snakemake.input[3]
 seq_id = snakemake.params.seq_id
 edge_id = snakemake.params.edge
 df = snakemake.params.global_dictionary
 
+
 # extract a dictionary of branch lengths keyed on the child node name of the corresponding branch
 def get_branch_lengths(input_tree_file):
     with open(input_tree_file, "r") as f:
-        tree_nwk = f.readlines()[0]
+        tree_nwk = f.readlines()[0].strip()
     input_tree = Tree(tree_nwk)
     lengths = {}
     ctr = 0
@@ -20,20 +22,22 @@ def get_branch_lengths(input_tree_file):
         ctr += 1
     return lengths
 
+
 def get_taxon_likelihood(input_file):
     likelihood = 0
     ll_str = "Log-likelihood of the tree: "
     with open(input_file, "r") as f:
-        for line in f.readlines():
+        for line in f.readlines()[0]:
             if ll_str in line:
                 likelihood = line.split(ll_str)[-1].split(" ")[0]
                 break
     return likelihood
 
+
 # return the distance to the closest leaf of the taxon specified
 def calculate_taxon_height(input_tree_file, taxon_name):
     with open(input_tree_file, "r") as f:
-        tree_nwk = f.readlines()[0]
+        tree_nwk = f.readlines()[0].strip()
     input_tree = Tree(tree_nwk)
     taxon = input_tree & taxon_name
     taxon_parent = taxon.get_ancestors()[0]
@@ -41,10 +45,23 @@ def calculate_taxon_height(input_tree_file, taxon_name):
     return taxon_parent.get_closest_leaf()[1]
 
 
+def get_distance_to_full_tree(small_tree_file, full_tree_file):
+    with open(small_tree_file, "r") as f:
+        small_tree_nwk = f.readlines()[0].strip()
+    small_tree = Tree(small_tree_nwk)
+    with open(full_tree_file, "r") as f:
+        full_tree_nwk = f.readlines()[0].strip()
+    full_tree = Tree(full_tree_nwk)
+    print(small_tree)
+    print(full_tree)
+    return small_tree.robinson_foulds(full_tree, unrooted_trees=True)[0]
+
+
 branchlengths = get_branch_lengths(tree_file)
 taxon_height = calculate_taxon_height(tree_file, seq_id)
 likelihood = get_taxon_likelihood(ml_file)
+rf_distance = get_distance_to_full_tree(tree_file, full_tree_file)
 # rf distance to full tree (need to make full tree file into a params for this rule)
 
 # df is actually a dictionary whose values are lists, each list should be a row in the dataframe
-df[seq_id] = [branchlengths, taxon_height, likelihood]
+df[seq_id] = [branchlengths, taxon_height, likelihood, rf_distance]
