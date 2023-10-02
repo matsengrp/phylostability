5f38da4acf4b08648ff74bffc2cadf79eff1c782
lenacoll <lena@lenacoll.de>
2023-10-02 14:30:09 -0700

    aggregate plots

--- scripts/create_plots.py
+++ scripts/create_plots.py
@@ -20,58 +20,43 @@ taxon_tii_list = [
 sorted_taxon_tii_list = sorted(taxon_tii_list, key=lambda x: x[1])
 
 
+def aggregate_taxon_edge_dfs(csv_list):
+    dfs = []
+    for csv_file in csv_list:
+        taxon_df = pd.read_csv(csv_file)
+        dfs.append(taxon_df)
+    df = pd.concat(dfs, ignore_index=True)
+    df = df.rename(columns={0:"seq_id"})
+    return df
+
+
 def edpl_vs_tii_scatterplot(taxon_df, filepath):
     sns.scatterplot(data=taxon_df, x="tii", y="edpl")
     plt.savefig(filepath)
     plt.clf()
 
 
-def get_plot_layout(num_plots):
-    """
-    This function takes as input the number of plots we want in one pdf and returns
-    fig,axes for subplots created with matplotlib.pyplot, so that there are in total
-    num_plots subplots.
-    If num_plots > 10, we get 5 plots per row, otherwise 3.
-    """
-    if num_plots > 10:
-        num_cols = 5
-    else:
-        num_cols = 3
-    num_rows = (num_plots + num_cols - 1) // num_cols  # Calculate the number of rows
-    fig, axes = plt.subplots(nrows=num_rows, ncols=num_cols, figsize=(10, 8))
-    # Flatten the axes array for easier indexing
-    axes = axes.flatten()
-    # Remove any extra subplots
-    for i in range(num_plots, num_rows * num_cols):
-        fig.delaxes(axes[i])
-    return fig, axes
-
-
-def likelihood_swarmplots(sorted_taxon_tii_list, taxon_edge_df_csv, filepath):
+def likelihood_swarmplots(sorted_taxon_tii_list, all_taxon_edge_df, filepath):
     """
     For each taxon, plot the likelihood of all optimised reattachments as swarmplot,
     sorted according to increasing TII
     """
-    # Create figure for likelihood swarmplots
-    num_plots = len(sorted_taxon_tii_list)
-    fig, axes = get_plot_layout(num_plots)
+    all_taxon_edge_df['seq_id'] = pd.Categorical(all_taxon_edge_df['seq_id'], 
+                                         categories=[pair[0] for pair in sorted(sorted_taxon_tii_list, key=lambda x: x[1])], 
+                                         ordered=True)
+    all_taxon_edge_df = all_taxon_edge_df.sort_values('seq_id')
+    plt.figure(figsize=(10, 6))  # Adjust figure size if needed
+    sns.swarmplot(data=all_taxon_edge_df, x='seq_id', y='likelihood')
 
-    # Iterate through CSV files and taxon names
-    for i, (taxon_name, tii) in enumerate(sorted_taxon_tii_list):
-        # Find the corresponding CSV file
-        csv_file = [file for file in taxon_edge_df_csv if taxon_name in file][0]
-        df = pd.read_csv(csv_file)
-        likelihood_values = df["likelihood"]
+    # Set labels and title
+    plt.xlabel('taxa (sorted by TII)')
+    plt.ylabel('Likelihood')
+    plt.title('swarmplot of likelihood vs. taxa sorted by TII')
 
-        sns.swarmplot(data=likelihood_values, ax=axes[i])
-        axes[i].set_title(f"{taxon_name}")
-        axes[i].set_xlabel(f"TII = {tii}")
-        axes[i].set_ylabel("Likelihood")
-        axes[i].set_xticklabels([])
-        axes[i].xaxis.set_ticks_position("none")
+    plt.xticks(range(len(sorted_taxon_tii_list)), [pair[1] for pair in sorted(sorted_taxon_tii_list, key=lambda x: x[1])])
+    plt.xticks(rotation=45)
 
     plt.tight_layout()
-    # Save the plot to pdf
     plt.savefig(filepath)
     plt.clf()
 
@@ -84,72 +69,88 @@ def seq_distance_swarmplot(distance_filepath, sorted_taxon_tii_list, plot_filepa
     distances = pd.read_table(
         distance_filepath, skiprows=[0], header=None, delim_whitespace=True, index_col=0
     )
-    fig, axes = get_plot_layout(len(sorted_taxon_tii_list))
-    # iteratively add swarmplot of distances
-    for i, (taxon_name, tii) in enumerate(sorted_taxon_tii_list):
-        sns.swarmplot(data=distances.loc[taxon_name].to_list(), ax=axes[i])
-        axes[i].set_title(f"{taxon_name}")
-        axes[i].set_xlabel(f"TII = {tii}")
-        axes[i].set_ylabel("Sequence distance to pruned taxon")
-        axes[i].set_xticklabels([])
-        axes[i].xaxis.set_ticks_position("none")
 
+    # Add seq_id as a column
+    distances["seq_id"] = distances.index
+
+    # Reshape the DataFrame into long format
+    df_long = pd.melt(distances, id_vars=['seq_id'], var_name='variable', value_name='value')
+
+    # Create the swarmplot
+    plt.figure(figsize=(10, 6))
+    sns.swarmplot(data=df_long, x='seq_id', y='value')  # Specify y='value'
+
+    # Set labels and title
+    plt.xlabel('taxa (sorted by TII)')
+    plt.ylabel('distances')
+    plt.title('swarmplot of sequence distances vs. taxa sorted by TII')
+
+    # Set x-axis ticks and labels
+    plt.xticks(range(len(sorted_taxon_tii_list)), [pair[1] for pair in sorted(sorted_taxon_tii_list, key=lambda x: x[1])], rotation=45)
     plt.tight_layout()
-    # Save the plot to pdf
     plt.savefig(plot_filepath)
     plt.clf()
 
 
-def bootstrap_swarmplot(reduced_tree_files, plot_filepath):
+def bootstrap_swarmplot(reduced_tree_files, sorted_taxon_tii_list, plot_filepath):
     """
     For each taxon, plot the bootstrap support of nodes in the tree inferred on the
     reduced alignment as swarmplot, sorted according to increasing TII
     """
+    bootstrap_df = []
     for treefile in reduced_tree_files:
         with open(treefile, "r") as f:
             tree = Tree(f.readlines()[0].strip())
-        bootstrap_list = []
+        seq_id = treefile.split("/")[-2]
+        tii = [p[1] for p in sorted_taxon_tii_list if p[0]==seq_id][0]
         for node in tree.traverse("postorder"):
             if not node.is_leaf() and not node.is_root():
-                bootstrap_list.append(int(node.support))
-        fig, axes = get_plot_layout(len(tree) + 1)
-        # iteratively add swarmplot of distances
-        for i, (taxon_name, tii) in enumerate(sorted_taxon_tii_list):
-            sns.swarmplot(data=bootstrap_list, ax=axes[i])
-            axes[i].set_title(f"{taxon_name}")
-            axes[i].set_xlabel(f"TII = {tii}")
-            axes[i].set_ylabel("Bootstrap support")
-            axes[i].set_xticklabels([])
-            axes[i].xaxis.set_ticks_position("none")
+                bootstrap_df.append([seq_id, node.support, tii])
+    bootstrap_df = pd.DataFrame(bootstrap_df, columns=["seq_id", "bootstrap_support", "tii"])
+    bootstrap_df = bootstrap_df.sort_values('tii')
+
+    plt.figure(figsize=(10, 6))  # Adjust figure size if needed
+    sns.swarmplot(data=bootstrap_df, x = "seq_id", y = "bootstrap_support")
+
+    # Set labels and title
+    plt.xlabel('taxa (sorted by TII)')
+    plt.ylabel('bootstrap support')
+    plt.title('swarmplot of bootstrap support vs. taxa sorted by TII')
+
+    plt.xticks(range(len(sorted_taxon_tii_list)), [pair[1] for pair in sorted(sorted_taxon_tii_list, key=lambda x: x[1])])
+    plt.xticks(rotation=45)
 
     plt.tight_layout()
-    # Save the plot to pdf
     plt.savefig(plot_filepath)
     plt.clf()
 
 
-def taxon_height_swarmplot(taxon_edge_df_csv, sorted_taxon_tii_list, plot_filepath):
-    num_plots = len(sorted_taxon_tii_list)
-    fig, axes = get_plot_layout(num_plots)
+def taxon_height_swarmplot(all_taxon_edge_df, sorted_taxon_tii_list, plot_filepath):
+    """
+    For each taxon, plot the height of the reattachment for all possible reattachment
+    edges as a swarmplot vs its TII values
+    """
+    all_taxon_edge_df['seq_id'] = pd.Categorical(all_taxon_edge_df['seq_id'], 
+                                         categories=[pair[0] for pair in sorted(sorted_taxon_tii_list, key=lambda x: x[1])], 
+                                         ordered=True)
+    all_taxon_edge_df = all_taxon_edge_df.sort_values('seq_id')
+    plt.figure(figsize=(10, 6))  # Adjust figure size if needed
+    sns.swarmplot(data=all_taxon_edge_df, x='seq_id', y='likelihood')
 
-    # Iterate through CSV files and taxon names
-    for i, (taxon_name, tii) in enumerate(sorted_taxon_tii_list):
-        # Find the corresponding CSV file
-        csv_file = [file for file in taxon_edge_df_csv if taxon_name in file][0]
-        df = pd.read_csv(csv_file)
+    # Set labels and title
+    plt.xlabel('taxa (sorted by TII)')
+    plt.ylabel('Likelihood')
+    plt.title('swarmplot of likelihood vs. taxa sorted by TII')
 
-        sns.swarmplot(data=df["taxon_height"], ax=axes[i])
-        axes[i].set_title(f"{taxon_name}")
-        axes[i].set_xlabel(f"TII = {tii}")
-        axes[i].set_ylabel("Reattachment height")
-        axes[i].set_xticklabels([])
-        axes[i].xaxis.set_ticks_position("none")
+    plt.xticks(range(len(sorted_taxon_tii_list)), [pair[1] for pair in sorted(sorted_taxon_tii_list, key=lambda x: x[1])])
+    plt.xticks(rotation=45)
 
     plt.tight_layout()
-    # Save the plot to pdf
     plt.savefig(plot_filepath)
     plt.clf()
 
+
+all_taxon_edge_df = aggregate_taxon_edge_dfs(taxon_edge_df_csv)
 
 # plot edpl vs TII for each taxon
 edpl_filepath = os.path.join(plots_folder, "edpl_vs_tii.pdf")
@@ -157,7 +158,7 @@ edpl_vs_tii_scatterplot(taxon_df, edpl_filepath)
 
 # swarmplot likelihoods of reattached trees for each taxon, sort by TII
 ll_filepath = os.path.join(plots_folder, "likelihood_swarmplots.pdf")
-likelihood_swarmplots(sorted_taxon_tii_list, taxon_edge_df_csv, ll_filepath)
+likelihood_swarmplots(sorted_taxon_tii_list, all_taxon_edge_df, ll_filepath)
 
 # swarmplot sequence distances from mldist files for each taxon, sort by TII
 seq_distance_filepath = os.path.join(plots_folder, "seq_distance_vs_tii.pdf")
@@ -169,9 +170,9 @@ seq_distance_swarmplot(
 
 # swarmplot bootstrap support reduced tree for each taxon, sort by TII
 bootstrap_plot_filepath = os.path.join(plots_folder, "bootstrap_vs_tii.pdf")
-bootstrap_swarmplot(reduced_tree_files, bootstrap_plot_filepath)
+bootstrap_swarmplot(reduced_tree_files, sorted_taxon_tii_list, bootstrap_plot_filepath)
 
 taxon_height_plot_filepath = os.path.join(plots_folder, "taxon_height_vs_tii.pdf")
 taxon_height_swarmplot(
-    taxon_edge_df_csv, sorted_taxon_tii_list, taxon_height_plot_filepath
+    all_taxon_edge_df, sorted_taxon_tii_list, taxon_height_plot_filepath
 )
