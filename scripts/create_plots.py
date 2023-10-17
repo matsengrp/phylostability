3f152fd3b3da43b1b1f38e51f6c3bc87eeda82a3
Lena Collienne <lena@lenacoll.de>
2023-10-17 16:21:11 -0700

    plots for seq vs tree distance and distances between reattachment and siblings in tree

--- scripts/create_plots.py
+++ scripts/create_plots.py
@@ -72,6 +72,135 @@ def aggregate_and_filter_by_likelihood(taxon_edge_csv_list, p, hard_threshold=3)
     return df
 
 
+def get_best_reattached_tree(seq_id, all_taxon_edge_df, data_folder):
+    """
+    Return best reattached tree with seq_id (best means highest likelihood)
+    of reattachment edge as given in all_taxon_edge_df.
+    """
+    filtered_df = all_taxon_edge_df.loc[all_taxon_edge_df["seq_id"] == seq_id]
+    best_edge_id = filtered_df.loc[filtered_df["likelihood"].idxmin()][0]
+    best_edge_id = best_edge_id.split("_")[-1]
+    tree_filepath = (
+        data_folder
+        + "reduced_alignments/"
+        + seq_id
+        + "/reduced_alignment.fasta_add_at_edge_"
+        + str(best_edge_id)
+        + ".nwk_branch_length.treefile"
+    )
+    tree = Tree(tree_filepath)
+    return tree
+
+
+def get_best_reattached_tree_distances_to_seq_id(
+    seq_id, all_taxon_edge_df, data_folder
+):
+    """ "
+    Return distance matrix for distances in tree with highest likelihood
+    among all reattached trees (as per all_taxon_edge_df).
+    """
+    tree = get_best_reattached_tree(seq_id, all_taxon_edge_df, data_folder)
+    leaves = tree.get_leaf_names()
+    # Compute distance for each leaf to seq_id leaf
+    distances = {}
+    for i, leaf in enumerate(leaves):
+        if leaf != seq_id:
+            distances[leaf] = tree.get_distance(leaf, seq_id)
+    return distances
+
+
+def plot_seq_and_tree_dist_diff(
+    all_taxon_edge_df, sorted_taxon_tii_list, mldist_file, data_folder, plot_filepath
+):
+    """
+    Plot difference between distance of seq_id to other sequences in alignment
+    and corresponding distance in reattached treefor f in mldist_file.
+    """
+    ml_distances = pd.read_table(
+        mldist_file,
+        skiprows=[0],
+        header=None,
+        delim_whitespace=True,
+        index_col=0,
+    )
+    ml_distances.columns = ml_distances.index
+    distance_diffs = []
+    for seq_id, tii in sorted_taxon_tii_list:
+        reattached_distance = get_best_reattached_tree_distances_to_seq_id(
+            seq_id, all_taxon_edge_df, data_folder
+        )
+        q = np.quantile(list(reattached_distance.values()), 0.25)
+        for seq in reattached_distance:
+            if reattached_distance[seq] < q:
+                diff = reattached_distance[seq] - ml_distances[seq_id][seq]
+                distance_diffs.append([seq_id + str(tii), diff])
+
+    df = pd.DataFrame(distance_diffs, columns=["seq_id", "distance_diff"])
+    sns.stripplot(data=df, x="seq_id", y="distance_diff")
+    plt.axhline(0, color="red")
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.savefig(plot_filepath)
+    plt.clf()
+
+
+def plot_distance_reattachment_sibling(
+    all_taxon_edge_df, sorted_taxon_tii_list, mldist_file, data_folder, plot_filepath
+):
+    """
+    If S is the subtree that is sibling of reattached sequence, we plots ratio of
+    average distance of sequences in S and sequences in S's sibling S' in reduced
+    tree to average distance of reattached sequences and sequences in S'.
+    """
+    ml_distances = pd.read_table(
+        mldist_file,
+        skiprows=[0],
+        header=None,
+        delim_whitespace=True,
+        index_col=0,
+    )
+    ml_distances.columns = ml_distances.index
+    distances = []
+    for seq_id, tii in sorted_taxon_tii_list:
+        # TODO: Ideally we look at the distances within the tree rather than the sequence
+        # distance(?). This does however require knowing optimised branch lengths in the
+        # reattached tree!
+        tree = get_best_reattached_tree(seq_id, all_taxon_edge_df, data_folder)
+        reattachment_node = tree.search_nodes(name=seq_id)[0].up
+        if reattachment_node.is_root():
+            # for now we ignore reattachments just under the root
+            continue
+        sibling = [node for node in reattachment_node.children if node.name != seq_id][
+            0
+        ]
+        sibling_cluster = sibling.get_leaf_names()
+        siblings_sibling = [
+            node for node in reattachment_node.up.children if node != reattachment_node
+        ][0]
+        siblings_sibling_cluster = siblings_sibling.get_leaf_names()
+        avg_sibling_distance = 0
+        avg_new_node_distance = 0
+        for leaf1 in siblings_sibling_cluster:
+            avg_new_node_distance += ml_distances[leaf1][seq_id]
+            for leaf2 in sibling_cluster:
+                avg_sibling_distance += ml_distances[leaf1][leaf2]
+        avg_sibling_distance /= len(sibling_cluster) * len(siblings_sibling_cluster)
+        avg_new_node_distance /= len(siblings_sibling_cluster)
+        distances.append(
+            [seq_id + " " + str(tii), avg_sibling_distance, "sibling_distance"]
+        )
+        distances.append(
+            [seq_id + " " + str(tii), avg_new_node_distance, "new_node_distance"]
+        )
+
+    df = pd.DataFrame(distances, columns=["seq_id", "distances", "class"])
+    sns.stripplot(data=df, x="seq_id", y="distances", hue="class")
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.savefig(plot_filepath)
+    plt.clf()
+
+
 def edpl_vs_tii_scatterplot(taxon_df, filepath):
     sns.scatterplot(data=taxon_df, x="tii", y="edpl")
     plt.savefig(filepath)
@@ -588,7 +717,9 @@ def bootstrap_and_bts_plot(
 
     # Now, you can plot directly:
     plt.figure(figsize=(10, 6))
-    sns.stripplot(data=local_bootstrap_df, x="seq_id", y="bootstrap_support")
+    sns.stripplot(
+        data=local_bootstrap_df, x="seq_id", y="bootstrap_support", hue="location"
+    )
 
     # X-axis labels: Use the sorted taxon-TII pairs to label the x-ticks
     plt.xticks(
@@ -887,6 +1018,7 @@ reduced_tree_files = snakemake.input.reduced_trees
 full_tree_file = snakemake.input.full_tree
 mldist_file = snakemake.input.mldist_file
 plots_folder = snakemake.params.plots_folder
+data_folder = snakemake.params.data_folder
 reattachment_distance_csv = snakemake.input.reattachment_distance_csv
 reattachment_distance_topological_csv = (
     snakemake.input.reattachment_distance_topological_csv
@@ -905,18 +1037,44 @@ all_taxon_edge_df = aggregate_and_filter_by_likelihood(taxon_edge_df_csv, 0.02, 
 # all_taxon_edge_df = aggregate_taxon_edge_dfs(taxon_edge_df_csv)
 print("Done reading data.")
 
-# print("Start plotting mldists.")
-# mldist_plot_filepath = os.path.join(plots_folder, "mldist_ratio.pdf")
-# mldist_closest_taxa_plot_filepath = os.path.join(
-#     plots_folder, "mldist_closest_taxa_comparisons.pdf"
-# )
-# mldist_plots(
-#     mldist_file,
-#     sorted_taxon_tii_list,
-#     mldist_plot_filepath,
-#     mldist_closest_taxa_plot_filepath,
-# )
-# print("Done plotting mldists.")
+
+print("Start plotting sequence and tree distance differences.")
+distance_diff_filepath = os.path.join(plots_folder, "distance_diff.pdf")
+plot_seq_and_tree_dist_diff(
+    all_taxon_edge_df,
+    sorted_taxon_tii_list,
+    mldist_file,
+    data_folder,
+    distance_diff_filepath,
+)
+print("Done plotting sequence and tree distance differences.")
+
+
+print("Start plotting distances between addded sequence and siblings of reattachment.")
+distance_reattachment_sibling_filepath = os.path.join(
+    plots_folder, "distance_reattachment_sibling.pdf"
+)
+plot_distance_reattachment_sibling(
+    all_taxon_edge_df,
+    sorted_taxon_tii_list,
+    mldist_file,
+    data_folder,
+    distance_reattachment_sibling_filepath,
+)
+print("Done plotting distances between addded sequence and siblings of reattachment.")
+
+print("Start plotting mldists.")
+mldist_plot_filepath = os.path.join(plots_folder, "mldist_ratio.pdf")
+mldist_closest_taxa_plot_filepath = os.path.join(
+    plots_folder, "mldist_closest_taxa_comparisons.pdf"
+)
+mldist_plots(
+    mldist_file,
+    sorted_taxon_tii_list,
+    mldist_plot_filepath,
+    mldist_closest_taxa_plot_filepath,
+)
+print("Done plotting mldists.")
 
 
 # plot branch length distance of reattachment locations vs TII, hue = log_likelihood
