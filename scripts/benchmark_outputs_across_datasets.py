643263f0490e670b638a21cc5dfdebf423ed0a38
lenacoll <lena@lenacoll.de>
2023-12-19 11:23:44 -0800

    fix benchmarking output plots

--- scripts/benchmark_outputs_across_datasets.py
+++ scripts/benchmark_outputs_across_datasets.py
@@ -12,17 +12,21 @@ plots_folder = snakemake.params.plots_folder
 def extract_rule_names_from_df(df_name):
     df = pd.read_csv(df_name)
     return df.rule.unique().tolist()
+
+
 rule_names = extract_rule_names_from_df(df_names[0])
 
 
-values_to_plot = {"dataset": [],
-                  "# sequences": [],
-                  "sequence length": [],
-                  "CPU time": [],
-                  "time in seconds" : []}
+values_to_plot = {
+    "dataset": [],
+    "# sequences": [],
+    "sequence length": [],
+    "CPU time": [],
+    "time in seconds": [],
+}
 for dfn in df_names:
     df = pd.read_csv(dfn).fillna(0)
-    dset = dfn.split("/")[1]
+    dset = dfn.split("/")[2]
     fastan = [f for f in fasta_names if dset in f][0]
     with open(fastan, "r") as f:
         ctr = 0
@@ -42,20 +46,29 @@ for dfn in df_names:
 df = pd.DataFrame(values_to_plot)
 mx = df["# sequences"].max() if df["# sequences"].max() != 0 else 1
 mn = df["# sequences"].min()
-df["normalized # sequences"] = [20*2**((x-mn)/mx) for x in df["# sequences"]]
+df["normalized # sequences"] = [20 * 2 ** ((x - mn) / mx) for x in df["# sequences"]]
+
 
 def plot_total_value_breakdown(df, colname, plot_path):
     plt.figure(figsize=(10, 6))
-    sns.scatterplot(data=df.sort_values(colname), x="dataset", y=colname, hue="sequence length", size="# sequences")
+    sns.scatterplot(
+        data=df.sort_values(colname),
+        x="dataset",
+        y=colname,
+        hue="sequence length",
+        size="# sequences",
+    )
     plt.xticks(
         rotation=90,
     )
     plt.title(colname + " breakdown over all datasets")
     plt.xlabel("data set")
     plt.ylabel(colname)
+    plt.legend(bbox_to_anchor=(1.05, 1), loc="upper left", borderaxespad=0)
     plt.tight_layout()
     plt.savefig(plot_path)
     plt.clf()
+
 
 plot_filepath = os.path.join(plots_folder, "CPU_time_breakdown_across_all_datasets.pdf")
 plot_total_value_breakdown(df, "CPU time", plot_filepath)
