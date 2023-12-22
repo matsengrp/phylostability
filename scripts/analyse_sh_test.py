2db31cb56435ed2cf056f81a4e325fc0b2aa0c76
lenacoll <lena@lenacoll.de>
2023-12-22 14:35:02 -0800

    print all taxa with low AU, even those with same reattached topologies as the one iqtrree randomly chose

--- scripts/analyse_sh_test.py
+++ scripts/analyse_sh_test.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from ete3 import Tree
 
 
 iqtree_files = snakemake.input.iqtree_file
 tree_order_files = snakemake.input.reattached_trees_order
 summary_statistics = snakemake.input.summary_statistics
+reattached_trees = snakemake.input.reattached_trees
 plot_filepath = snakemake.output.plot
 subdirs = snakemake.params.subdirs
 
@@ -92,11 +94,65 @@ for subdir in subdirs:
     df["seq_id"] = order
     df = df.merge(ss_df, on="seq_id", how="left")
     df["ID"] = df["dataset"] + " " + df["seq_id"] + " " + df["tii"].astype(str)
+    filtered_df = df[df["p-AU"] < 0.05]
     df_list.append(df)
+
+    # iqtree deletes duplicate trees. If the tree with p-value < 0.05 is a duplicate,
+    # we also want to add the remaining trees to the plot.
+    reattached_tree_file = [f for f in reattached_trees if subdir + "/" in f][0]
+    topology_id_map = {}
+    id_to_topology = {}
+    # get for each tree topology in reattached_tree_file all seq_ids for which reattached
+    # trees have that topology (get maps in both ways)
+
+    with open(order_file, "r") as ids_file, open(
+        reattached_tree_file, "r"
+    ) as trees_file:
+        for id_line, tree_line in zip(ids_file, trees_file):
+            tree_id = id_line.strip()
+            newick_tree = Tree(tree_line.strip())
+            tree_string = newick_tree.write(
+                format=9
+            )  # Convert the tree to a string representation
+            if tree_string not in topology_id_map:
+                topology_id_map[tree_string] = []
+            topology_id_map[tree_string].append(tree_id)
+            id_to_topology[id] = tree_string
+    # collect topologies that are present in filtere_df
+    topologies_of_interest = set()
+    for tree_id in filtered_df["ID"]:
+        for topology, ids in topology_id_map.items():
+            if tree_id in ids:
+                topologies_of_interest.add(topology)
+                break
+    # collect ids that match the topologies in filtered_df
+    matching_ids = set(
+        [topology_id_map[topology] for topology in topologies_of_interest]
+    )
+    # Creating a copy to avoid modifying the original DataFrame while iterating
+    new_rows = filtered_df.copy()
+
+    for id in matching_ids:
+        # Get the topology for this ID
+        topology = id_to_topology[id]
+
+        # Find a row in filtered_df with the same topology
+        for original_id in topology_id_map[topology]:
+            if original_id in filtered_df["ID"].values:
+                tii = original_id.split(" ")[-1]
+                # Copy the row and replace the ID
+                row_copy = filtered_df[filtered_df["ID"] == original_id].copy()
+                row_copy["ID"] = iqtree_file.split("/")[-2] + " " + id + " " + str(tii)
+                new_rows = new_rows.append(row_copy, ignore_index=True)
+                break
+
+    # Now new_rows contains the original rows plus the new rows
+    filtered_df = new_rows
+
 
 big_df = pd.concat(df_list, ignore_index=True)
 
-filtered_df = big_df[big_df["p-AU"] < 0.05]
+
 plt.figure(figsize=(10, 6))
 sns.scatterplot(filtered_df, x="ID", y="normalised_tii")
 plt.xticks(rotation=90)
