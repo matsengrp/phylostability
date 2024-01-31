b319aeba5ec9b72be36473b74bbe6e0a42c50689
lenacoll <lena@lenacoll.de>
2024-01-31 10:08:09 -0800

    fix decimals for pie plot labels

--- scripts/plot_au_test_classifier.py
+++ scripts/plot_au_test_classifier.py
@@ -2,6 +2,8 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import math
+
 from sklearn.metrics import confusion_matrix, auc, ConfusionMatrixDisplay
 
 
@@ -42,6 +44,28 @@ def plot_random_forest_classifier_results(results_csv, roc_csv, plot_filepath):
 
 
 def plot_au_test_pie_chart(df, plot_filepath):
+    """
+    Pie plot for TII = 0 vs != 0 and siginficance of AU-test
+    """
+
+    def custom_format(values):
+        # convert input values to floats with as many decimals as necessary
+        # to get a non-zero decimal
+        # Default: one decimal
+        min_value = min(values)
+        new_values = []
+        for value in values:
+            if value == 0:
+                return "0%"
+            else:
+                # Determine the number of decimal places needed
+                decimal_places = abs(int(math.floor(math.log10(abs(value)))))
+                if decimal_places == 0:
+                    decimal_places += 1
+                format_string = "{{:.{}f}}%".format(decimal_places)
+                new_values.append(format_string.format(value))
+        return new_values
+
     # Pie Chart
     condition1 = ((df["normalised_tii"] == 0.0) & (df["p-AU"] < 0.05)).sum()
     condition2 = ((df["normalised_tii"] > 0.0) & (df["p-AU"] < 0.05)).sum()
@@ -51,12 +75,13 @@ def plot_au_test_pie_chart(df, plot_filepath):
     # Data to plot
     sizes = [condition1, condition2, condition3, condition4]
     total = sum(sizes)
-    percentages = [100 * (size / total) for size in sizes]
+    percentages = custom_format([100 * (size / total) for size in sizes])
+
     labels = [
-        "stable and significant ({:.1f}%)".format(percentages[0]),
-        "unstable and significant ({:.1f}%)".format(percentages[1]),
-        "stable and non-significant ({:.1f}%)".format(percentages[2]),
-        "unstable and non-significant ({:.1f}%)".format(percentages[3]),
+        "stable and significant ({})".format(percentages[0]),
+        "unstable and significant ({})".format(percentages[1]),
+        "stable and non-significant ({})".format(percentages[2]),
+        "unstable and non-significant ({})".format(percentages[3]),
     ]
     # Colors
     colors = [dark2.colors[i] for i in range(3, -1, -1)]
@@ -64,7 +89,7 @@ def plot_au_test_pie_chart(df, plot_filepath):
     explode = (0.1, 0, 0, 0)
 
     # Plot
-    plt.figure(figsize=(10, 8))
+    plt.figure(figsize=(10, 5))
     plt.pie(
         sizes,
         explode=explode,
