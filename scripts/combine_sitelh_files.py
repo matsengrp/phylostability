e3b6b931cb8db5875a1c7b6a590a47a9952e2a24
lenacoll <lena@lenacoll.de>
2024-08-30 02:46:43 -0700

    avoid exponentil notation for  p-values + fix typo

--- scripts/combine_sitelh_files.py
+++ scripts/combine_sitelh_files.py
@@ -2,7 +2,7 @@ import sys
 import numpy as np
 
 f1 = snakemake.input.full_tree_sitelh
-f2 = snakemake.input.inferred_tree_sitelh
+f2 = snakemake.input.inferred_trees_sitelh
 output_filename = snakemake.output.both_trees_txtfile
 
 full_str = "Tree	-lnL	Site	-lnL\n"
@@ -10,13 +10,13 @@ with open(f1, "r") as f:
     lines = f.readlines()
     full_str += "\t".join(lines[0].split())+"\n"
     for site, val in enumerate(lines[1].split()[1:]):
-        full_str += "\t\t"+str(site + 1) + "\t" + str(-float(val.strip())) + "\n"
+        full_str += "\t\t"+str(site + 1) + "\t" + f"{-float(val.strip()):.10f}" + "\n"
 
 with open(f2, "r") as f:
     lines = f.readlines()
     full_str += "2\t"+str(lines[0].split()[-1])+"\n"
     for site, val in enumerate(lines[1].split()[1:]):
-        full_str += "\t\t"+str(site + 1) + "\t" + str(-float(val.strip())) + "\n"
+        full_str += "\t\t"+str(site + 1) + "\t" + f"{-float(val.strip()):.10f}" + "\n"
 
 with open(output_filename, "w") as f:
     f.write(full_str)
