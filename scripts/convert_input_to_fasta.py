97f6906cdc694665c707045386ccc63ed466e93e
marybarker <marybarker103@gmail.com>
2024-09-26 09:04:18 -0700

    deduplicate input fastas as well as input nexus files

--- scripts/convert_input_to_fasta.py
+++ scripts/convert_input_to_fasta.py
@@ -28,56 +28,51 @@ for subdir in [
     if f.is_dir() and "plot" not in f.path and "benchmarking" not in f.path
 ]:
     msa_file = subdir + "/full_alignment.fasta"
-    if os.path.exists(msa_file):
-        # if not is_alignment(msa_file):
-        #     print(f"full_alignment.fasta doesn't contain msa. Delete {subdir}")
-        #     shutil.rmtree(subdir)
-        #     continue
-        continue
-    # Search for Nexus and FASTA files
-    nexus_files = [
-        f for f in glob.glob(os.path.join(subdir, "*.nex")) if ".splits." not in f
-    ]
-    fasta_files = glob.glob(os.path.join(subdir, "*.fasta"))
-    # resolve symlinks
-    nexus_files = [
-        os.readlink(file) if os.path.islink(file) else file for file in nexus_files
-    ]
-    fasta_files = [
-        os.readlink(file) if os.path.islink(file) else file for file in fasta_files
-    ]
-    nexus_file = None
-    if len(fasta_files) == 0 and len(nexus_files) == 0:
-        print(f"No alignment files in {subdir}. Delete {subdir}.")
-        shutil.rmtree(subdir)
-        continue
-    if len(fasta_files) > 0:
-        fasta_file = fasta_files[0]
-        if not is_alignment(fasta_file):
-            print(f"{fasta_file} doesn't contain alignment. Delete {subdir}.")
-            shutil.rmtree(subdir)
-            continue
-        os.rename(fasta_file, msa_file)
-        print(f"Renamed {fasta_file} to {msa_file}")
-        continue
-
-    elif len(nexus_files) == 1:
-        # If there is only the .n.nex file, we convert that one
-        # Otherwise, we go to 'else' and convert the other nexus file
-        nexus_file = nexus_files[0]
-        if not is_alignment(nexus_file):
-            print(f"{nexus_file} doesn't contain alignment. Delete {subdir}.")
-            shutil.rmtree(subdir)
-            continue
-        SeqIO.convert(nexus_file, "nexus", msa_file, "fasta")
-    else:
-        nexus_file = [f for f in nexus_files if ".n." not in f and ".splits." not in f][
-            0
+    fasta_file = msa_file
+    if not os.path.exists(msa_file):
+        # Search for Nexus and FASTA files
+        nexus_files = [
+            f for f in glob.glob(os.path.join(subdir, "*.nex")) if ".splits." not in f
         ]
-        if not is_alignment(nexus_file):
+        fasta_files = glob.glob(os.path.join(subdir, "*.fasta"))
+        # resolve symlinks
+        nexus_files = [
+            os.readlink(file) if os.path.islink(file) else file for file in nexus_files
+        ]
+        fasta_files = [
+            os.readlink(file) if os.path.islink(file) else file for file in fasta_files
+        ]
+        nexus_file = None
+        if len(fasta_files) == 0 and len(nexus_files) == 0:
+            print(f"No alignment files in {subdir}. Delete {subdir}.")
             shutil.rmtree(subdir)
             continue
-        SeqIO.convert(nexus_file, "nexus", msa_file, "fasta")
+        if len(fasta_files) > 0:
+            fasta_file = fasta_files[0]
+            if not is_alignment(fasta_file):
+                print(f"{fasta_file} doesn't contain alignment. Delete {subdir}.")
+                shutil.rmtree(subdir)
+                continue
+            os.rename(fasta_file, msa_file)
+            print(f"Renamed {fasta_file} to {msa_file}")
+
+        elif len(nexus_files) == 1:
+            # If there is only the .n.nex file, we convert that one
+            # Otherwise, we go to 'else' and convert the other nexus file
+            nexus_file = nexus_files[0]
+            if not is_alignment(nexus_file):
+                print(f"{nexus_file} doesn't contain alignment. Delete {subdir}.")
+                shutil.rmtree(subdir)
+                continue
+            SeqIO.convert(nexus_file, "nexus", msa_file, "fasta")
+        else:
+            nexus_file = [f for f in nexus_files if ".n." not in f and ".splits." not in f][
+                0
+            ]
+            if not is_alignment(nexus_file):
+                shutil.rmtree(subdir)
+                continue
+            SeqIO.convert(nexus_file, "nexus", msa_file, "fasta")
 
     # If sequences are labelled by integers, we update those labels to s_1, s_2, ...
     # to make sure that we don't get problems with reading trees with bootstrap support
@@ -90,12 +85,13 @@ for subdir in [
         except ValueError:
             return False
 
-    # Read the FASTA file
-    if nexus_file != None:
-        records = list(SeqIO.parse(nexus_file, "nexus"))
-    else:
-        # This is if we already had fasta file beforehand
-        records = list(SeqIO.parse(fasta_file, "fasta"))
+    ## Read the FASTA file
+    #if nexus_file != None:
+    #    records = list(SeqIO.parse(nexus_file, "nexus"))
+    #else:
+    #    # This is if we already had fasta file beforehand
+    #    records = list(SeqIO.parse(fasta_file, "fasta"))
+    records = list(SeqIO.parse(msa_file, "fasta"))
 
     # Make sure "-" is only gap character in sequences
     for record in records:
